91824e25e0582ba4785062621de50b719c1dae81
Kirk Roerig <mr.possoms@gmail.com>
2022-02-13 17:44:23 -0700

    Needs cleanup and rework, basic simulation working

--- sim/src/main.cpp
+++ sim/src/main.cpp
@@ -1,25 +1,3 @@
-/*************************************************************************
- *                                                                       *
- * Open Dynamics Engine, Copyright (C) 2001,2002 Russell L. Smith.       *
- * All rights reserved.  Email: russ@q12.org   Web: www.q12.org          *
- *                                                                       *
- * This library is free software; you can redistribute it and/or         *
- * modify it under the terms of EITHER:                                  *
- *   (1) The GNU Lesser General Public License as published by the Free  *
- *       Software Foundation; either version 2.1 of the License, or (at  *
- *       your option) any later version. The text of the GNU Lesser      *
- *       General Public License is included with this library in the     *
- *       file LICENSE.TXT.                                               *
- *   (2) The BSD-style license that is included with this library in     *
- *       the file LICENSE-BSD.TXT.                                       *
- *                                                                       *
- * This library is distributed in the hope that it will be useful,       *
- * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the files    *
- * LICENSE.TXT and LICENSE-BSD.TXT for more details.                     *
- *                                                                       *
- *************************************************************************/
-
 #include <ode/ode.h>
 #include <drawstuff/drawstuff.h>
 
@@ -33,64 +11,109 @@
 #define dsDrawLine dsDrawLineD
 #endif
 
+#define WHEEL_COUNT 1
+
+#define CUBE_FRAME_MASS 0.25
 
 dWorldID world;
-dSpaceID space;
-dBodyID body1;
-dBodyID body2;
-dJointID joint1, joint2;
+dSpaceID space, cube_space;
+
+dGeomID ground;
+dBodyID cube_body;
+dBodyID wheel_bodies[WHEEL_COUNT];
+dGeomID wheel_geoms[WHEEL_COUNT];
+dJointID wheel_axels[WHEEL_COUNT];
+dJointGroupID contactgroup;
+
+static const dVector3 xunit = {1, 0, 0}, yunit = {0, 1, 0}, zunit = {0, 0, 1};
+
+static void nearCallback (void *, dGeomID o1, dGeomID o2)
+{
+  int i,n;
+
+  // only collide things with the ground
+  int g1 = (o1 == ground);
+  int g2 = (o2 == ground);
+
+  if (!(g1 ^ g2)) return;
+
+  const int N = 10;
+  dContact contact[N];
+  n = dCollide (o1,o2,N,&contact[0].geom,sizeof(dContact));
+  if (n > 0)
+  {
+    for (i=0; i<n; i++)
+    {
+        contact[i].surface.mode = dContactSlip1 | dContactSlip2 |
+        dContactSoftERP | dContactSoftCFM | dContactApprox1;
+        contact[i].surface.mu = dInfinity;
+        contact[i].surface.slip1 = 0.1;
+        contact[i].surface.slip2 = 0.1;
+        contact[i].surface.soft_erp = 0.5;
+        contact[i].surface.soft_cfm = 0.3;
+        dJointID c = dJointCreateContact (world,contactgroup,&contact[i]);
+        dJointAttach (c,
+            dGeomGetBody(contact[i].geom.g1),
+            dGeomGetBody(contact[i].geom.g2));
+    }
+  }
+}
 
 void start()
 {
-    world = dWorldCreate();
-    dWorldSetGravity (world,0,0,-9.8);
-
-    dWorldSetDamping(world, 1e-4, 1e-5);
-//    dWorldSetERP(world, 1);
-
-    space = dSimpleSpaceCreate (0);
-
-    body1 = dBodyCreate(world);
-    body2 = dBodyCreate(world);
-
-    dBodySetPosition(body1, 0, 0, 3);
-    dBodySetPosition(body2, 0, 0, 1);
-
-
     dGeomID g;
     dMass mass;
 
-    g = dCreateBox(space, 0.2, 0.2, 1);
-    dGeomSetBody(g, body1);
-    dMassSetBox(&mass, 1, 0.2, 0.2, 1);
-    dBodySetMass(body1, &mass);
+    world = dWorldCreate();
+    space = dHashSpaceCreate (0);
 
-    g = dCreateBox(space, 0.2, 0.2, 1);
-    dGeomSetBody(g, body2);
-    dMassSetBox(&mass, 1, 0.2, 0.2, 1);
-    dBodySetMass(body2, &mass);
+    dWorldSetGravity (world,0,0,-9.8);
+    dWorldSetDamping(world, 1e-4, 1e-5);
 
-    joint1 = dJointCreateDBall(world, 0);
-    dJointAttach(joint1, body1, 0);
-    dJointSetDBallAnchor1(joint1, 0, 0, 3.5);
-    dJointSetDBallAnchor2(joint1, 0, 0, 4.5);
+    ground = dCreatePlane (space, 0, 0, 1, 0);
 
-    joint2 = dJointCreateDBall(world, 0);
-    dJointAttach(joint2, body1, body2);
-    dJointSetDBallAnchor1(joint2, 0, 0, 2.5);
-    dJointSetDBallAnchor2(joint2, 0, 0, 1.5);
+    contactgroup = dJointGroupCreate(0);
 
+    cube_body = dBodyCreate(world);
+    g = dCreateBox(space, 0.1, 0.1, 0.1);
+    dGeomSetBody(g, cube_body);
+    dMassSetBox(&mass, 0.5, 0.1, 0.1, 0.1);
+    dBodySetMass(cube_body, &mass);
+    dBodySetPosition(cube_body, 0, 0, 1);
+
+    // create each wheel
+    for (unsigned i = 0; i < WHEEL_COUNT; i++)
+    {
+        wheel_bodies[i] = dBodyCreate(world);
+        wheel_geoms[i] = dCreateCylinder(space, 0.04, 0.02);
+        dGeomSetBody(wheel_geoms[i], wheel_bodies[i]);
+        dMassSetCylinder(&mass, 7.85, 1, 0.04, 0.02);
+        dBodySetMass(wheel_bodies[i], &mass);
+        dBodySetPosition(wheel_bodies[i], -0.05, 0, 1);
+
+        dQuaternion q;
+        dQFromAxisAndAngle (q, 0, 1, 0, M_PI*0.5);
+        dBodySetQuaternion (wheel_bodies[i], q);
+
+        // create joint and attach to cube
+        wheel_axels[i] = dJointCreateHinge2(world,0);
+        dJointAttach(wheel_axels[i], wheel_bodies[0], cube_body);
+        const dReal* a = dBodyGetPosition(cube_body);
+        dJointSetHinge2Anchor(wheel_axels[i], a[0], a[1], a[2]);
+        dJointSetHinge2Axes(wheel_axels[i], zunit, xunit);
+        dJointSetHinge2Param(wheel_axels[i],dParamLoStop,0);
+        dJointSetHinge2Param(wheel_axels[i],dParamHiStop,0);
+    }
 
     // initial camera position
-    float xyz[3] = {3.8966, -2.0614, 4.0300};
-    float hpr[3] = {153.5, -16.5, 0};
+    float xyz[3] = {-0.6174,-0.8838,1.0300};
+    float hpr[3] = {54.0000,-40.0000,0.0000};
     dsSetViewpoint (xyz,hpr);
 }
 
 void stop()
 {
     dSpaceDestroy(space);
-
     dWorldDestroy(world);
 }
 
@@ -98,6 +121,9 @@ void stop()
 void drawGeom(dGeomID g)
 {
     int gclass = dGeomGetClass(g);
+
+    if (gclass == dPlaneClass) { return; }
+
     const dReal *pos = dGeomGetPosition(g);
     const dReal *rot = dGeomGetRotation(g);
 
@@ -116,61 +142,62 @@ void drawGeom(dGeomID g)
             dsDrawBox(pos, rot, lengths);
             break;
         }
+        case dCylinderClass:
+        {
+            dReal rad, len;
+            dsSetColorAlpha(1, 0, 0, 1);
+            dsSetTexture (DS_WOOD);
+            dGeomCylinderGetParams(g, &rad, &len);
+            dsDrawCylinderD(pos, rot, len, rad);
+            break;
+        }
 
         default:
         {}
     }
 }
 
+void command (int cmd)
+{
+  switch (cmd) {
+  case 'j': case 'J':
+    dJointAddHinge2Torques(wheel_axels[0], 0, 0.01);
+    break;
+  case 'l': case 'L':
+    dJointAddHinge2Torques(wheel_axels[0], 0, -0.01);
+    break;
+  }
+}
+
 void simLoop(int pause)
 {
-    if (!pause) {
+    if (!pause)
+    {
 
         static dReal t = 0;
 
         const dReal step = 0.005;
         const unsigned nsteps = 4;
 
-        for (unsigned i=0; i<nsteps; ++i) {
-
-            dReal f = sin(t*1.2)*0.8;
-            dBodyAddForceAtRelPos(body1,
-                                  f, 0, 0,
-                                  0, 0, -0.5); // at the lower end
-
-            dReal g = sin(t*0.7)*0.8;
-            dBodyAddForceAtRelPos(body2,
-                                  0, g, 0,
-                                  0, 0, -0.5); // at the lower end
+        for (unsigned i=0; i<nsteps; ++i)
+        {
             t += step;
 
+            dSpaceCollide(space, nullptr, nearCallback);
             dWorldQuickStep(world, step);
+
+            // remove all contact joints
+            dJointGroupEmpty (contactgroup);
         }
     }
 
     // now we draw everything
     unsigned ngeoms = dSpaceGetNumGeoms(space);
-    for (unsigned i=0; i<ngeoms; ++i) {
+    for (unsigned i=0; i<ngeoms; ++i)
+    {
         dGeomID g = dSpaceGetGeom(space, i);
-
         drawGeom(g);
     }
-
-    dVector3 a11, a12;
-    dJointGetDBallAnchor1(joint1, a11);
-    dJointGetDBallAnchor2(joint1, a12);
-    dsSetColor(1, 0, 0);
-    dsDrawLine(a11, a12);
-
-    //printf("Error 1: %f\n", fabs(dJointGetDBallDistance(joint1) - dCalcPointsDistance3(a11, a12)));
-
-    dVector3 a21, a22;
-    dJointGetDBallAnchor1(joint2, a21);
-    dJointGetDBallAnchor2(joint2, a22);
-    dsSetColor(0, 1, 0);
-    dsDrawLine(a21, a22);
-
-    //printf("Error 2: %f\n", fabs(dJointGetDBallDistance(joint2) - dCalcPointsDistance3(a21, a22)));
 }
 
 
@@ -182,7 +209,7 @@ int main(int argc, char **argv)
     fn.version = DS_VERSION;
     fn.start = &start;
     fn.step = &simLoop;
-    fn.command = 0;
+    fn.command = &command;
     fn.stop = stop;
     fn.path_to_textures = DRAWSTUFF_TEXTURE_PATH;
 
