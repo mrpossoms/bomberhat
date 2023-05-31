7c0be38ccfbc4a133d60e9ff456255efc6be52a6
Kirk Roerig <mr.possoms@gmail.com>
2023-05-31 07:37:19 -0600

    Starting work on quat rotor

---
+++ sim/inc/scenario/reaction-wheels.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+struct reaction_wheels final : public simulation
+{
+    reaction_wheels() : simulation() {}
+
+    void setup() override;
+
+    void command(int cmd) override;
+
+    std::vector<dReal> observables() override;
+
+    void apply_action(const std::vector<dReal>& a) override;
+
+    void user_step(dReal step_size=0.005) override;
+};
